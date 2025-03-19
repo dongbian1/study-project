bfa781463d6c2e113f20ef9de18e4a6946da7589
陈佳鑫 <cjx>
2025-03-19 10:00:52 +0800

    修改打包配置

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import AutoImport from 'unplugin-auto-import/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/study-project',
   plugins: [
     vue(),
     WindiCSS(),
