d1bd0839ddfcfd923d25977f18053a3033d41c85
陈佳鑫 <cjx>
2025-03-19 09:48:45 +0800

    修改

--- deploy.sh
+++ deploy.sh
@@ -14,6 +14,6 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:dongbian1/study-project.git master:pages
+git push -f https://github.com/dongbian1/study-project.git pages
 
 cd -
