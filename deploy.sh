11517b40608372a5060d9db36d5b43cb9ffe3f05
陈佳鑫 <cjx>
2025-03-21 15:34:53 +0800

    修改静态部署

--- deploy.sh
+++ deploy.sh
@@ -14,6 +14,6 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f https://github.com/dongbian1/study-project.git pages
+git push -f https://github.com/dongbian1/study-project.git master:gh-pages
 
 cd -
